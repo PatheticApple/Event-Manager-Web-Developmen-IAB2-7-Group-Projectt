a4509cf2e4b6f0b5bfe4671fcf2796cd2d039735
Gary Nguyen <n11197633@qut.edu.au>
2023-10-08 17:53:13 +1000

    Created a database and also added some dummy data. Also made some changes to eventDetails.html and index.html to dynamically display the content

--- a3_starter_code-main/projectfile/website/views.py
+++ a3_starter_code-main/projectfile/website/views.py
@@ -1,10 +1,11 @@
 from flask import Blueprint, render_template
-
+from .models import User, Event, EventDetail, Booking, Comment, TicketType
 bp = Blueprint('main', __name__)
-
+from . import db
 
 @bp.route('/')
 def index():
-    # return '<h1>Starter code for the assessment<h1>'
-    return render_template('index.html')
+    events = db.session.scalars(db.select(Event)).all()
+    
+    return render_template('index.html', events = events)
 
