e4c7f5c3172aa9303e0928539b075e3e76a17838
Gary Nguyen <n11197633@qut.edu.au>
2023-10-26 19:49:03 +1000

    Booking events is completed

--- a3_starter_code-main/projectfile/website/eventEditor.py
+++ a3_starter_code-main/projectfile/website/eventEditor.py
@@ -3,6 +3,7 @@ from .models import Event, EventDetail, TicketType
 import os
 from . import db
 from werkzeug.utils import secure_filename
+from .forms import CommentForm
 from datetime import datetime,date
 
 #additional import:
@@ -50,6 +51,8 @@ def create_event():
     capacity = request.form['capacity']
     # availability = request.form['availability']
     filePath3 = request.files['file2']
+
+    status = request.form['status']
     filePath32 = check_upload_file(filePath3)
 
 
@@ -61,10 +64,10 @@ def create_event():
         
     # Get ticket types from the form
     # ticket_types = request.form.getlist('ticket_types[]')
-    if date2 < datetime.today():
-       status = "Inactive"
-    else:
-       status = "Open"
+    # if date2 < datetime.today():
+    #    status = "Inactive"
+    # else:
+    #    status = "Open"
     # Create Event object and add it to the database
 
     eventDetaiis = EventDetail(description = description, artistName = artist_name, capacity = capacity, availability = capacity, imagePath = filePath32)
@@ -94,3 +97,64 @@ def create_event():
 
     # Redirect to a success page or home page
     return redirect(url_for('main.index'))
+
+
+
+@editbp.route('/update_event/<id>', methods=['GET', 'POST'])
+@login_required
+def show2(id):
+    event = db.session.scalar(db.select(Event).where(Event.eventID==id))
+    eventDetails = db.session.scalar(db.select(EventDetail).where(EventDetail.eventID==id))
+    ticketDetails = db.session.query(TicketType).filter(TicketType.eventID == id).all()
+
+    return render_template('eventEditor2.html', event=event, eventDetails=eventDetails, ticketDetails=ticketDetails)
+
+
+
+@editbp.route('/update_event/<id>/update', methods=['GET', 'POST'])
+@login_required
+def update2(id):
+    print("ID:", id)
+    # Get form data
+    event_name = request.form['event_name']
+    suburb = request.form['suburb']
+    state = request.form['state']
+    date1 = request.form['date']
+    date2 = datetime.strptime(date1, '%Y-%m-%d')
+    genres = request.form['genres']
+    # filePath = request.files['file']
+    # filePath2 = check_upload_file(filePath)
+    artist_name = request.form['artist_name']
+    description = request.form['description']
+    capacity = request.form['capacity']
+    status = request.form['status']
+    # availability = request.form['availability']
+    # filePath3 = request.files['file2']
+    # filePath32 = check_upload_file(filePath3)
+
+
+    custom_ticket_names = request.form.getlist('custom_ticket_names[]')
+    custom_ticket_prices = request.form.getlist('custom_ticket_prices[]')
+    custom_ticket_quantities = request.form.getlist('custom_ticket_quantities[]')
+    
+
+    event = db.session.scalar(db.select(Event).where(Event.eventID==id))
+    eventDetails = db.session.scalar(db.select(EventDetail).where(EventDetail.eventID==id))
+    ticketDetails = db.session.query(TicketType).filter(TicketType.eventID == id).all()
+    cform = CommentForm() 
+    event.eventName = event_name
+    event.suburb = suburb
+    event.state = state
+    event.date = date2
+    event.genres = genres
+    event.status = status
+    eventDetails.artistName = artist_name
+    eventDetails.description = description
+
+    oldCapacity = int(eventDetails.capacity)
+    differenceCapacity = int(capacity) - oldCapacity 
+    eventDetails.capacity = capacity
+    eventDetails.availability += differenceCapacity
+    db.session.commit()
+
+    return redirect(url_for('details.show', id=id))
